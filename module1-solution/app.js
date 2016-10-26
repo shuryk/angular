69021203c18753518bae71056e7ed33e708edd36
Alex Verbitchi <shuryk1305@gmail.com>
2016-10-25 22:44:28 -0400

    angularjs setup

---
+++ module1-solution/app.js
@@ -0,0 +1,15 @@
+(function () {
+'use strict';
+
+    angular.module('LunchCheck', [])
+    .controller('LunchCheckController', LunchCheckController);    
+
+    LunchCheckController.$inject = ['$scope'];
+    function LunchCheckController($scope) {
+
+        $scope.lunchDishes = "";
+
+
+    }
+
+})();
