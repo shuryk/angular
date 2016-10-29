1fbda9d5966c9da53b4d1692ad26242f8eb0a312
Alex Verbitchi <shuryk1305@gmail.com>
2016-10-29 18:31:09 -0400

    Adds empty string validation

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -8,8 +8,36 @@
     function LunchCheckController($scope) {
 
         $scope.lunchDishes = "";
+        $scope.commentMsg = "Initial Comment!";
+
+        $scope.checkIfTooMuch = function() {
+            var elements = removeEmptyElements( $scope.lunchDishes.split(','));
+
+            console.log(elements);
+            console.log(elements.length);
 
 
+            if (elements.length == 0 ) {
+                $scope.commentMsg = "Please enter data first";
+            }
+            else if (elements.length <= 3) {
+                $scope.commentMsg = "Enjoy";
+            }
+            else if (elements.length > 3) {
+
+                $scope.commentMsg = "Too much!"; 
+            }
+        }
+
+        function removeEmptyElements(elements) {
+            var sanitizedElements = []
+            for(var i = 0; i < elements.length; i++) {
+                if(elements[i] != "") {
+                    sanitizedElements.push(elements[i]);
+                }                
+            }
+            return sanitizedElements;
+        }
     }
 
 })();
