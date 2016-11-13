b4f337daf229efb9463534c54b14d65e5f4e201d
Alex Verbitchi <shuryk1305@gmail.com>
2016-11-12 22:29:36 -0500

    Final version

--- module2-solution/app.js
+++ module2-solution/app.js
@@ -8,19 +8,102 @@ angular.module('ShoppingListCheckOff', [])
 
 ToBuyController.$inject = ['ShoppingListCheckOffService']
 function ToBuyController(ShoppingListCheckOffService){
+    var toBuy = this;
 
+    toBuy.items = ShoppingListCheckOffService.getItemsFomrShopingListToBuy();
+    console.log("toBuy.items", toBuy.items);
+
+    toBuy.markAsBought = function (index) {
+        ShoppingListCheckOffService.removeItemFromShopingListToBuy(index);
+        toBuy.isEmpty = ShoppingListCheckOffService.isToBuyListEmpty();
+    }
+
+    toBuy.isEmpty = ShoppingListCheckOffService.isToBuyListEmpty();
 }
 
 AlreadyBoughtController.$inject = ['ShoppingListCheckOffService']
 function AlreadyBoughtController(ShoppingListCheckOffService){
+    var alreadyBought = this;
+    alreadyBought.items = ShoppingListCheckOffService.getItemsFromShopingListAlreadyBought();
 
+    alreadyBought.markAsToBuy = function (index) {
+        ShoppingListCheckOffService.removeItemFromShopingListAlreadyBought(index);
+        alreadyBought.isEmpty =  ShoppingListCheckOffService.isAlreadyBoughtEmpty();
+    }
+
+    alreadyBought.isEmpty =  ShoppingListCheckOffService.isAlreadyBoughtEmpty();
 }
 
 
 function ShoppingListCheckOffService () {
 
-    var shopingListToBuy = [];
+    var service = this;
+
+    var shopingListToBuy = [
+        { name: "cookies", quantity: 10 },
+        { name: "nuts", quantity: '1 kg' },
+        { name: "milk", quantity: '1 litre' },
+        { name: "kiwi", quantity: 6 },
+        { name: "banana", quantity: 7 },
+        { name: "chips", quantity: '4 bags' },
+        { name: "tomatos", quantity: 4 }];
+        
     var shopingListAlreadyBought = [];
+
+
+    service.isToBuyListEmpty = function () {
+        if (shopingListToBuy.length == 0) {
+            return true;
+        }
+        return false;
+    }
+
+    service.isAlreadyBoughtEmpty = function () { 
+             
+        if (shopingListAlreadyBought.lenght > 0) {
+            return false;
+        }
+        else {
+            return true;
+        }        
+    }
+
+    service.addItemToShopingListToBuy = function (itemName, quantity) {
+        var item = {
+      name: itemName,
+      quantity: quantity
+    };
+     shopingListToBuy.push(item);
+    }
+
+    service.removeItemFromShopingListToBuy = function (itemIndex) {
+
+        console.log("remove item index", itemIndex);
+        var item = shopingListToBuy.splice(itemIndex, 1)[0];
+        console.log("remove item ", item);
+        service.addItemToShopingListAlreadyBought(item); 
+    }
+
+    service.getItemsFomrShopingListToBuy = function () {
+        console.log("shopingListToBuy", shopingListToBuy);
+
+        return shopingListToBuy;
+    }
+
+    service.addItemToShopingListAlreadyBought = function (item) {
+         shopingListAlreadyBought.push(item);
+    }
+
+    service.removeItemFromShopingListAlreadyBought = function (itemIndex) {
+        var item = shopingListAlreadyBought.splice(itemIndex, 1);
+         service.addItemToShopingListToBuy(item); 
+        
+    }
+
+    service.getItemsFromShopingListAlreadyBought = function () {
+
+        return shopingListAlreadyBought;
+    }
 }
 
 })();
